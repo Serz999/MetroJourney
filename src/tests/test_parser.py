1f60adad9b3e9e4d2f4357f0bf0ae721ff6a0bbf
serz999 <serz_999@mail.ru>
2023-04-04 13:10:12 +0300

    add tests frame

---
+++ src/tests/test_parser.py
@@ -0,0 +1,6 @@
+import pytest
+
+# Write here your tests...
+
+if __name__ == '__main__':
+    pytest.main()
---
+++ src/tests/test_trip.py
@@ -0,0 +1,7 @@
+import pytest
+
+# Write here your tests...
+
+if __name__ == '__main__':
+    pytest.main()
+
